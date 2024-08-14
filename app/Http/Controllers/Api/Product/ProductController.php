46f3eb44fdb9743991a5d122886abb29e4668a82
lu739 <kroshkalu739@gmail.com>
2024-08-14 14:54:33 +0300

    add product reviews

--- app/Http/Controllers/Api/Product/ProductController.php
+++ app/Http/Controllers/Api/Product/ProductController.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers\Api\Product;
 use App\Enum\ProductStatus;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        auth()->login(User::query()->where('is_admin', true)->inRandomOrder()->first());
+    }
+
     public function index()
     {
         $products = Product::query()
@@ -41,7 +48,7 @@ class ProductController extends Controller
             'rating' => $product->rating(),
             'price' => $product->price,
             'count' => $product->count,
-            'images' => $product->images()->get(),
+            'images' => $product->images()->get()->map(fn($image) => $image->path),
             'reviews' => $product->reviews->map(fn($review) => [
                 'id' => $review->id,
                 'userName' => $review->user->name,
@@ -49,5 +56,36 @@ class ProductController extends Controller
                 'text' => $review->text,
             ]),
         ];
+    }
+
+    public function store(Request $request)
+    {
+        $product = auth()->user()->products()->create([
+        // $product = Product::query()->create([
+            'name' => $request->str('name'),
+            'description' => $request->str('description'),
+            'price' => $request->input('price'),
+            'count' => $request->integer('count'),
+            'status' => $request->enum('status', ProductStatus::class),
+        ]);
+
+        foreach ($request->images as $image) {
+            $product->images()->create([
+                'path' => config('app.url') . Storage::url($image->storePublicly('images')),
+            ]);
+        }
+
+        return $product;
+    }
+
+    public function review(Request $request, Product $product)
+    {
+        $review = $product->reviews()->create([
+            'user_id' => auth()->id(),
+            'text' => $request->str('text'),
+            'rating' => $request->integer('rating'),
+        ]);
+
+        return $review->only('id');
     }
 }
