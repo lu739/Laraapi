3b64e3d6a283a75812b1c9d2294ef5bb320f64b8
lu739 <kroshkalu739@gmail.com>
2024-08-19 20:10:35 +0300

    add routes group and version

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::
-    middleware('auth:sanctum')->
-    apiResource('products',\App\Http\Controllers\Api\Product\ProductController::class);
-
-Route::
-    middleware('auth:sanctum')->
-    controller(\App\Http\Controllers\Api\Product\ProductController::class)
-    ->prefix('products')
-    ->group(function () {
-        Route::post('/{product}/review', 'addReview')->name('product.review.store');
+Route::prefix('v1/')->group(function () {
+    require __DIR__ . '/group_v1/product.php';
+    require __DIR__ . '/group_v1/user.php';
 });
 
-Route::controller(\App\Http\Controllers\Api\User\UserController::class)->group(function () {
-    Route::get('/login', 'login')->name('login');
-    Route::get('/register', 'register')->name('register');
-});
+// Route::prefix('v2/')->group(function () {
+//
+// });
